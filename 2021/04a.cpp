69c3c08c8361b14445de4ec11464d21d70a88490
György Straub <gyorgy@nuclearheart.com>
2021-12-07 20:12:51 +0000

    Added solutions for '21 days 4 & 5.

---
+++ 2021/04a.cpp
@@ -0,0 +1,115 @@
+#include "utils.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Specify filename as 1st param." << std::endl;
+		return 1;
+	}
+
+	std::ifstream file(argv[1]);
+
+	std::string line;
+	std::getline(file, line);
+
+	std::vector<uint32_t> drawn;
+	ReadInts<uint32_t>(line, [&drawn](auto n){
+		drawn.push_back(n);
+	});
+
+	struct Ticket
+	{
+		int32_t data[25];
+
+		void Read(std::ifstream& fin)
+		{
+			auto n = data;
+			std::string line;
+			for (int i = 0; i < 5; ++i)
+			{
+				std::getline(fin, line);
+				sscanf(line.c_str(), "%d %d %d %d %d", n, n + 1, n + 2, n + 3, n + 4);
+				n += 5;
+			}
+		}
+
+		bool Mark(int32_t number)
+		{
+			auto iFind = std::find(std::begin(data), std::end(data), number);
+			if (iFind != std::end(data))
+			{
+				*iFind = -1;
+
+				// check horizontally
+				const uint32_t c = std::distance(data, iFind);
+				const uint32_t x = c % 5;
+				const uint32_t y = c / 5;
+				uint32_t hits = 0;
+				for (int i = 0; i < 5; ++i)
+				{
+					hits += data[y * 5 + i] == -1;
+				}
+
+				if (hits == 5)
+				{
+					return true;
+				}
+
+				// check vertically
+				hits = 0;
+				for (int i = 0; i < 5; ++i)
+				{
+					hits += data[x + 5 * i] == -1;
+				}
+
+				if (hits == 5)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		uint32_t CalculateScore(uint32_t x) const
+		{
+			uint32_t score = 0;
+			for (auto n: data)
+			{
+				score += n * (n >= 0);
+			}
+			return score * x;
+		}
+	};
+
+	std::vector<Ticket> tickets;
+	while (std::getline(file, line))
+	{
+		Ticket t;
+		t.Read(file);
+
+		tickets.push_back(t);
+	}
+
+	auto result = 0;
+	bool done = false;
+	for (auto n: drawn)
+	{
+		for (auto& t: tickets)
+		{
+			if (t.Mark(n))
+			{
+				result = t.CalculateScore(n);
+				done = true;
+				break;
+			}
+		}
+
+		if (done) break;
+	}
+
+	std::cout << result << std::endl;
+
+	return 0;
+}
---
+++ 2021/05b.cpp
@@ -0,0 +1,41 @@
+#include "utils.h"
+#include <iostream>
+#include <map>
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Specify filename as 1st param." << std::endl;
+		return 1;
+	}
+
+	std::map<uint32_t, uint32_t> lines;
+	std::ifstream inFile(argv[1]);
+	ReadAllLines(inFile, [&lines](std::string line)
+	{
+		int x1, y1, x2, y2;
+		if (sscanf(line.c_str(), "%d,%d -> %d,%d", &x1, &y1, &x2, &y2) != 4)
+		{
+			std::cerr << "Failed to parse '" << line << "'" << std::endl;
+		}
+		else
+		{
+			int dx = ((x2 - x1) > 0) - ((x2 - x1) < 0);
+			int dy = ((y2 - y1) > 0) - ((y2 - y1) < 0);
+			x2 += dx;
+			y2 += dy;
+			for (; !(x1 == x2 && y1 == y2); x1 += dx, y1 += dy)
+			{
+				++lines[(y1 << 16) + x1];
+			}
+		}
+	});
+
+        uint32_t result = std::count_if(lines.begin(), lines.end(), [](auto const& x){
+                return x.second > 1;
+        });
+        std::cout << result << std::endl;
+
+	return 0;
+}
