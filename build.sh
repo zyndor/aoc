873b84d09006622b1de290710832e277067b668e
György Straub <gyorgy@nuclearheart.com>
2021-12-13 08:14:43 +0000

    Added support for compiling C#, to build.sh.

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-g++ $1 --std=c++17 -I$(dirname $0) -Wall -Wfatal-errors $CXXFLAGS
+#!/bin/bash
+ext=${1##*.}
+
+case $ext in
+	cpp)
+	g++ $1 --std=c++17 -I$(dirname $0) -Wall -Wfatal-errors $CXXFLAGS
+	;;
+
+	cs)
+	mcs $1 -out:a.out
+	;;
+
+	*)
+	echo Failed to match extension: $ext
+	exit 1
+	;;
+esac
